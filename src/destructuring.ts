d2d5e92e3c161f5322fd464daf71b0b015c74097
Ayon203008 <paypal572874@gmail.com>
2025-11-16 15:55:22 +0600

    typeallias added

---
+++ src/destructuring.ts
@@ -0,0 +1,24 @@
+const user ={
+    id:21321,
+    name:{
+        firstNmae:"Abid",
+        lastName:"hasan"
+    },
+    gender:"male"
+}
+
+const name = user.name
+const firstName= user.name.firstNmae
+
+console.log(name)
+console.log(firstName)
+
+
+
+
+
+
+
+
+
+
---
+++ src/typeAlias.ts
@@ -0,0 +1,31 @@
+type user ={
+    id:number;
+    name:{
+        firstName: string;
+        lastName:string
+    },
+    gender:"male"|"female";
+    contactNumber:string;
+    address:{
+        division:string;
+        city:string
+    }
+}
+
+
+const user1:user={
+    id:4345354,
+    name:{
+        firstName:"Abid ",
+        lastName:"hasan "
+    },
+    gender:"male";
+    contactNumber:"Melano park , silicon valley";
+    address:{
+        division:"NY";
+        city:"La"
+    }
+}
+
+
+console.log(user1)
